3c3e48d3389acbe88834f286ab4c04b8dd45da3b
alejo688dev <alejandroramirez688@gmail.com>
2020-11-14 22:15:24 -0500

    Se agrega ejercicio 1

---
+++ Ejercicio_1.py
@@ -0,0 +1,21 @@
+# Premisa de ejercicio
+# Leer un número entero y mostrar todos los enteros comprendidos entre 1 y el número leído
+
+def conteo(numero):
+    numero += 1
+    for i in range(1, numero):
+        print(i)
+
+if __name__ == "__main__":
+    print("Bienvenido al programa de números enteros")
+
+    numero = int(input("Ingrese un número igual o mayor a uno por favor: "))
+
+    if numero < 1:
+        while numero < 1:
+            print("Error número no válido")
+            numero = int(input("Ingrese un número igual o mayor a uno por favor: "))
+            
+        conteo(numero)
+    else:
+        conteo(numero)
