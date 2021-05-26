2e624863e0e84a5b4a3476411e35b43ecbcfbed2
alejo688 <alejandroramirez688@gmail.com>
2021-05-25 22:50:57 -0500

    Se adiciona funcion en el ejercicio 4 y se crea el ejercicio5

--- Ejercicio_4.py
+++ Ejercicio_4.py
@@ -1,11 +1,7 @@
 # Premisa de ejercicio 4
 # Leer dos números y mostrar todos los enteros comprendidos entre ellos.
 
-if __name__ == "__main__":
-    print("Bienvenido al programa de números")
-    numero1 = int(input("Por favor ingrese el primero número: "))
-    numero2 = int(input("Por favor ingrese el segundo número: "))
-
+def numeros(numero1, numero2):
     if numero2 > numero1:
         for i in range(numero1, numero2 + 1):
             print(i)
@@ -15,3 +11,9 @@ if __name__ == "__main__":
     else:
         print(numero1)
 
+if __name__ == "__main__":
+    print("Bienvenido al programa de números")
+    numero1 = int(input("Por favor ingrese el primero número: "))
+    numero2 = int(input("Por favor ingrese el segundo número: "))
+
+    numeros(numero1, numero2)
